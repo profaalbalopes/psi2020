3166469390cd8696c4096b2964ff930916e74358
profaalbalopes <78361086+profaalbalopes@users.noreply.github.com>
2021-02-02 11:41:58 -0300

    Inicializar o repositório com o arquivo index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+  <head>
+    <title>Exemplo de código em repositório</title>
+  </head>
+  
+  <body>
+    <h1>
+      <?php echo "Esse é um exemplo de código que vai ser enviado para o repositório"; ?>
+    </h1:>
+  </body>
+</html>
