28a41abfe3d0a411e7438b0a7a29c16f2888ff40
profaalbalopes <78361086+profaalbalopes@users.noreply.github.com>
2021-02-02 11:47:06 -0300

    Inserção de imagem no arquivo index.php

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
       <?php echo "Esse é um exemplo de código que vai ser enviado para o repositório"; ?>
     </h1>
     
+    
+    <p>
+    <img src="http://tribodoci.net/wp-content/uploads/2013/07/top-5-scm-vitrine-300x212.png" />
+    </p>
+    
   </body>
 </html>
