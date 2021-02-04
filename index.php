897a68bcca96a07361b9abe5d6a656570d5eb48e
profaalbalopes <78361086+profaalbalopes@users.noreply.github.com>
2021-02-04 11:37:40 -0300

    Correção na página index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <img src="http://tribodoci.net/wp-content/uploads/2013/07/top-5-scm-vitrine-300x212.png" />
     </p>
     
-    <p>a href="novapagina.php">Ir para Nova Página</a></p>
+    <p><a href="novapagina.php">Ir para Nova Página</a></p>
     
   </body>
 </html>
